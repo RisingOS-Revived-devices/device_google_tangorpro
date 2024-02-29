8bb8258ea56e8a77cffe9ba948192ff84999182d
Ravi Jain <raviajain@google.com>
2024-02-29 13:28:44 +0000

    dumpstate: touch: Init using touch_predump
    More info at go/touch_predump
    
    Test: Test locally on test device and check predump_touch works.
    Bug: 287907308
    Change-Id: If949310a0721177a5a860f9f0d46698982d1279e

--- device-tangorpro.mk
+++ device-tangorpro.mk
@@ -62,7 +62,7 @@ DISABLE_TELEPHONY_EUICC := true
 
 include device/google/tangorpro/audio/tangorpro/audio-tables.mk
 include device/google/gs201/device-shipping-common.mk
-include device/google/gs-common/touch/gti/gti.mk
+include device/google/gs-common/touch/gti/predump_gti.mk
 include device/google/gs-common/touch/nvt/nvt.mk
 include device/google/gs-common/led/led.mk
 include device/google/gs-common/wlan/dump.mk
