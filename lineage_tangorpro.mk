b3a920c992d5b818e32589971506eb9a66ac2f10
Austin Young <austineyoung2000@gmail.com>
2025-05-08 19:02:58 -0400

    Update fingerprint/build description from BP1A.250505.005
    Change-Id: I36c24cad103ceac2b390c5888c6f0b07d2685454

--- lineage_tangorpro.mk
+++ lineage_tangorpro.mk
@@ -52,8 +52,8 @@ WITH_PIXEL-STYLE := true
 RISING_MAINTAINER=EliteDarkKaiser
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="tangorpro-user 15 BP1A.250305.020.T2 13023825 release-keys" \
-    BuildFingerprint=google/tangorpro/tangorpro:15/BP1A.250305.020.T2/13023825:user/release-keys \
+    BuildDesc="tangorpro-user 15 BP1A.250505.005 13277524 release-keys" \
+    BuildFingerprint=google/tangorpro/tangorpro:15/BP1A.250505.005/13277524:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
